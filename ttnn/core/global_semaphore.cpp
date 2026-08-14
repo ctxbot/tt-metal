b036a5ed8ff0aafe80945f5699042a9be2bceaf4
Oleksandr Bacherikov <obacherikov@tenstorrent.com>
2026-08-14 23:03:58 +0000

    GlobalSemaphore creation update

--- ttnn/core/global_semaphore.cpp
+++ ttnn/core/global_semaphore.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <tt-metalium/host_api.hpp>
 #include <tt-metalium/global_semaphore.hpp>
+#include <tt_stl/assert.hpp>
 #include <tt_stl/span.hpp>
 
 namespace ttnn::global_semaphore {
@@ -91,12 +92,13 @@ MultiDeviceGlobalSemaphore create_global_semaphore_with_same_address(
 
 GlobalSemaphore create_global_semaphore(
     IDevice* device, const CoreRangeSet& cores, uint32_t initial_value, BufferType buffer_type) {
-    return CreateGlobalSemaphore(device, cores, initial_value, buffer_type);
+    return GlobalSemaphore(device, cores, initial_value, buffer_type);
 }
 
 GlobalSemaphore create_global_semaphore(
     MeshDevice* mesh_device, const CoreRangeSet& cores, uint32_t initial_value, BufferType buffer_type) {
-    return CreateGlobalSemaphore(mesh_device, cores, initial_value, buffer_type);
+    TT_FATAL(mesh_device != nullptr, "MeshDevice cannot be null");
+    return GlobalSemaphore(*mesh_device, cores, initial_value, buffer_type);
 }
 
 tt::tt_metal::DeviceAddr get_global_semaphore_address(const GlobalSemaphore& global_semaphore) {
